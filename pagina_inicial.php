d9c6fc860ecdea7c5800a0c9381d8caee2df5403
juliafortunat24 <juliafortunat24@gmail.com>
2024-10-17 15:18:14 -0300

    Create pagina_inicial.php
    criação de página inicial

---
+++ pagina_inicial.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <a href="historia_antiga.php">História Antiga</a>
+  <a href="historia_primitiva.php">Historia Primitiva</a>
+  <a href="idade_moderna.php">Idade Moderna</a>
+  <a href="idade_media.php">Idade Media </a>
+  <a href="idade_contemporanea.php">Idade Contemporanea</a>
+  <p>teste</p>
+</body>
+</html>
