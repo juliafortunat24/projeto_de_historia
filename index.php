e4b2e7f6873662038b466bc4094eb0ca40a05523
juliafortunat24 <juliafortunat24@gmail.com>
2024-10-22 10:17:31 -0300

    juliaaaa

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login - História</title>
     <link rel="stylesheet" href="./projeto de historia/index.css">
 </head>
 <body>
