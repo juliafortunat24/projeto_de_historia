83850bac04f7a4bda2c7e680d5d442f6f265631c
juliafortunat24 <juliafortunat24@gmail.com>
2024-11-07 09:02:23 -0300

    commit julia

--- paginas/aluno.php
+++ paginas/aluno.php
@@ -11,7 +11,8 @@ if ($_SESSION['usuario_sessao']=="" && $_SESSION['tipo_sessao']=="") {
   <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>DOCUMENTO</title>
+      <title>Pagína Inicial</title>
+      <link rel="icon" href="../img/img_para_colocar_no_title-removebg-preview.png" type="image/x-icon">
       <link rel="stylesheet" href="aluno.css">
       <link href="../css/aluno.css" rel="stylesheet">
       </head>
