d17274ac08388b80e360de604aeb49196287ed06
CibelySantos <168744739+CibelySantos@users.noreply.github.com>
2024-11-06 21:55:03 -0300

    ...

--- bd/logout.php
+++ bd/logout.php
@@ -1,8 +1,12 @@
 <?php
-session_start(); 
+session_start();
 
-session_unset(); 
-session_destroy(); 
+session_unset();
+session_destroy();
+
+header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+header("Pragma: no-cache"); // HTTP 1.0.
+header("Expires: 0"); // Proxies.
 
 header("Location: ../index.php");
 exit();
--- paginas/administrador.php
+++ paginas/administrador.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-header("Cache-Control: no-cache, no-store, must-revalidate");
-header("Pragma: no-cache");
-header("Expires: 0");
+header("Cache-Control: no-cache, no-store, must-revalidate"); 
+header("Pragma: no-cache"); 
+header("Expires: 0"); 
 
 if (!isset($_SESSION['nome_usuario'])) {
     header("Location: ../index.php");
