ba9623b897dd1638499b2cb9f446273bc76994ae
juliafortunat24 <juliafortunat24@gmail.com>
2024-11-07 08:23:09 -0300

    commit - mudança nos titles

--- paginas/idadeprimitiva.php
+++ paginas/idadeprimitiva.php
@@ -51,7 +51,8 @@ if (isset($_SESSION['tipo_sessao'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Idade Primitiva</title>
+    <link rel="icon" href="../img/img_para_colocar_no_title-removebg-preview.png" type="image/x-icon">
     <link rel="stylesheet" href="../css/idadeprimitiva.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
